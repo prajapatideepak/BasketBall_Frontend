939fe259ec7ae942ea5a46986d7bef4fe935319a
shadrajput <shadrajput6@gmail.com>
2023-01-26 00:33:05 +0530

    ok

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -43,7 +43,7 @@ function Login() {
                 <img src={image} alt="landing" className="" />
             </div>
 
-            <div className=' 2xl:w-2/6 '>
+            <div className=' 2xl:w-[27%] px-5 mt-4  '>
                 <div className=' space-y-3  '>
                     <h1 className='text-3xl font-bold text-center uppercase'>Login</h1>
                     <p className=' text-gray-500 text-center text-xs sm:text-base '>Wellcome back! Please enter your details.</p>
@@ -52,62 +52,65 @@ function Login() {
                     <img src={google} alt="" className='w-7' />
                     <p>Log in with Google</p>
                 </div>
-                    <div className='flex justify-center items-center'>
-                        Or
-                    </div>
-                <div className="">
-                    <form action="" className='space-y-3' onSubmit={handleSubmit}>
-                        <div className='space-y-5 py-4 '>
-                            <div className='space-y-2'>
-                                <label htmlFor="Email" className='font-semibold text-base'>Email</label>
-                                <input type="Email"
-                                    value={values.email}
-                                    placeholder='Enter Your Email'
-                                    autoComplete='off'
-                                    name='email'
-                                    id='email'
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className='w-full rounded-md py-2 px-3 outline-non border border-slate-300 focus:outline-blue-500' />
-                                {errors.email && touched.email
-                                    ?
-                                    <p className='form-error text-red-600 text-sm font-semibold'>{errors.email}</p>
-                                    :
-                                    null}
-                            </div>
-                            <div className='space-y-2'>
-                                <label htmlFor="Password" className='font-semibold text-base'>Password</label>
-                                <input type="password"
-                                    value={values.password}
-                                    placeholder='Enter Your Password'
-                                    autoComplete='off'
-                                    name='password'
-                                    id='password'
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className='w-full rounded-md py-2 px-3 outline-non border border-slate-300 focus:outline-blue-500' />
-                                {errors.password && touched.password
-                                    ?
-                                    <p className='form-error text-red-600 text-sm font-semibold'>{errors.password}</p>
-                                    :
-                                    null}
-                            </div>
-                        </div>
-                        <Link to={"/ForgetPassword"}>
-                            <div className=' flex justify-end items-end'>
-                                <h1 className='text-sm underline font-semibold text-[#ee6730] cursor-pointer'>Forget Password?</h1>
-                            </div>
-                        </Link>
-                        <div className='py-1'>
-
-                            <button type='submit' className='py-2  uppercase bg-[#ee6730] hover:shadow-none hover:border-[#ee6730] border-[#ee6730] border hover:bg-white hover:text-[#ee6730] duration-500 shadow-sm font-sans shadow-[#ee6730] px-20 rounded-md w-full text-white font-semibold text-base'>
-                                Submit
-                            </button>
-                        </div>
-                    </form>
-
-
+                <div className='flex justify-center items-center'>
+                    Or
                 </div>
+                <form action="" className='space-y-3' onSubmit={handleSubmit}>
+                    <div className='space-y-5 py-4 '>
+                        <div className='space-y-2'>
+                            <label htmlFor="Email" className='font-semibold text-base'>Email</label>
+                            <input type="Email"
+                                value={values.email}
+                                placeholder='Enter Your Email'
+                                autoComplete='off'
+                                name='email'
+                                id='email'
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className='w-full rounded-md py-2 px-3 outline-non border border-slate-300 focus:outline-blue-500' />
+                            {errors.email && touched.email
+                                ?
+                                <p className='form-error text-red-600 text-sm font-semibold'>{errors.email}</p>
+                                :
+                                null}
+                        </div>
+                        <div className='space-y-2'>
+                            <label htmlFor="Password" className='font-semibold text-base'>Password</label>
+                            <input type="password"
+                                value={values.password}
+                                placeholder='Enter Your Password'
+                                autoComplete='off'
+                                name='password'
+                                id='password'
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className='w-full rounded-md py-2 px-3 outline-non border border-slate-300 focus:outline-blue-500' />
+                            {errors.password && touched.password
+                                ?
+                                <p className='form-error text-red-600 text-sm font-semibold'>{errors.password}</p>
+                                :
+                                null}
+                        </div>
+                    </div>
+                    <Link to={"/ForgetPassword"}>
+                        <div className=' flex justify-end items-end'>
+                            <h1 className='text-sm underline font-semibold text-[#ee6730] cursor-pointer'>Forget Password?</h1>
+                        </div>
+                    </Link>
+                    <div className='py-1'>
+
+                        <button type='submit' className='py-2  uppercase bg-[#ee6730] hover:shadow-none hover:border-[#ee6730] border-[#ee6730] border hover:bg-white hover:text-[#ee6730] duration-500 shadow-sm font-sans shadow-[#ee6730] px-20 rounded-md w-full text-white font-semibold text-base'>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+                <div className='text-center mt-5 text-slate-600'>
+                    Don't have an account?
+                    <Link to={"/register"}>
+                        <span className='text-black font-semibold cursor-pointer hover:text-[#ee6730] px-1'>Sign up for free</span>
+                    </Link>
+                </div>
+
             </div>
         </div>
     )
