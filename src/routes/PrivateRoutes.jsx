1e71c005679ea7205ea02568fa8771b1b269fa79
sadikali karadiya <karadiyasadikali@gmail.com>
2023-01-30 22:09:45 +0530

    merge complete

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,12 +1,9 @@
 import { lazy } from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
 import PrivateLayout from '../layouts/PrivateLayout'
-import Dashboard from '../pages/Dashboard'
 import TeamAddEdit from '../pages/TeamAddEdit'
 import TeamProfile from '../pages/TeamProfile'
 import TeamProfileDetail from '../pages/TeamProfileDetail'
-import { Route, Routes, Navigate } from "react-router-dom";
-import PrivateLayout from "../layouts/PrivateLayout";
 import AboutUs from "../pages/AboutUs";
 import ContactUs from "../pages/ContactUs";
 import Dashboard from "../pages/Dashboard";
@@ -14,20 +11,17 @@ import Gallery from "../pages/Gallery";
 import News from "../pages/News";
 import Player from "../pages/Player";
 import Role from "../pages/Role";
-import TeamRegistration from "../pages/TeamRegistration";
 
 const PrivateRoutes = () => {
   return (
     <Routes>
       <Route element={<PrivateLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/team-registration" element={<TeamRegistration />} />
         <Route path="/role" element={<Role />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="news/*" element={<News />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="player/*" element={<Player />} />
-        <Route path='/dashboard' element={<Dashboard />}/>
         <Route path='/team-add-edit' element={<TeamAddEdit />}/>
         <Route path='/team-profile' element={<TeamProfile />}/>
         <Route path='/team-profile-detail/:team_id' element={<TeamProfileDetail />}/>
