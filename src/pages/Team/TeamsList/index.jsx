cb569781dc41b902111db2b625d11038816fe87b
sadikali karadiya <karadiyasadikali@gmail.com>
2023-02-03 16:30:08 +0530

    pagination component added

--- src/pages/Team/TeamsList/index.jsx
+++ src/pages/Team/TeamsList/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import TeamCard from '../../../Component/TeamCard'
 import Heading from '../../../Component/Heading'
+import Paginate from '../../../Component/Pagination';
+import { AiOutlineTeam } from 'react-icons/ai';
 
 function TeamsList() {
-    const allTeams = [
+
+    //--------pagination variables-------
+    const [paginationData, setPaginationData] = React.useState([]);
+    //------------------------------------
+
+    const all_teams = [
         {
             team_id: 1001,
             team_logo: '/CBL_Images/basketball_team_logo_1.webp',
@@ -23,8 +30,11 @@ function TeamsList() {
             matches_played: 12,
             matches_won: 8,
             matches_lost: 4
-        }
+        },
     ]
+    const [allTeams, setAllTeams] = React.useState(all_teams);
+
+
   return (
     <section className='min-h-screen'>
         <div className="mx-auto px-10 py-12 sm:px-20 sm:py-12 md:px-20 md:py-16 lg:px-24 xl:px-28 2xl:px-32">
@@ -32,16 +42,21 @@ function TeamsList() {
                 <Heading text="All Teams" />
             </div>
             {
-                allTeams.length > 0
+                paginationData.length > 0
                 ?
-                    allTeams.map((team, i) =>{
+                    paginationData.map((team, i) =>{
                         return(
                             <TeamCard key={i} teamDetails={team}/>
                         )                        
                     })
                 :
-                    <h3>No team found</h3>
+                    <div className='flex justify-center items-center mt-16 md:mt-24'>
+                        <AiOutlineTeam className="text-2xl xs:text-3xl sm:text-5xl text-gray-400 mr-2"/>
+                        <p className='text-xs xs:text-sm sm:text-lg font-medium text-gray-400'>No Team Found</p>
+                    </div>
             }
+            
+            <Paginate data={allTeams} setPaginationData={setPaginationData} itemsPerPage={1} />
         </div>
     </section>
   )
