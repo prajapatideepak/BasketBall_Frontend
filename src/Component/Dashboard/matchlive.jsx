8807b01b78a6a15ec16336bde38b6c4acbc62929
shadrajput <shadrajput6@gmail.com>
2023-02-17 12:41:50 +0530

    ok

---
+++ src/Component/Dashboard/matchlive.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
+
+function MatchLive() {
+  const slides = [
+    {
+      url: 'https://resources.prod.fiba.pulselive.com/photo-resources/2022/06/17/a349e782-c409-4516-b660-adc0a83dee84/Africa-WCQW3-Banner.jpg?height=1080&width=1920',
+    },
+    {
+      url: 'https://resources.prod.fiba.pulselive.com/photo-resources/2022/06/17/867faf69-cbb1-4e53-8cd6-2d68420f368f/Europe-WCQW3-Banner.jpg?height=1080&width=1920',
+    },
+    // {
+    //   url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+    // },
+
+    // {
+    //   url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+    // },
+    // {
+    //   url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+    // },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  return (
+    <div className=' h-[500px] w-full  py-0 my-5 top-4 relative  group'>
+     <div className='bg-black/20 absolute h-full min-w-full rounded-2xl'>
+        jdf
+    </div>
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+      ></div>
+      {/* Left Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-4 text-2xl rounded-full p-2 bg-black/20 duration-300 hover:bg-black text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-4 text-2xl rounded-full p-2 bg-black/20 duration-300 hover:bg-black text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className='flex top-4 justify-center py-2'>
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className='text-2xl cursor-pointer'
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MatchLive;
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import MatchLive from "../../Component/Dashboard/matchlive";
+import img from "../../../public/CBL_Images/nba.png"
 
 const Dashboard = () => {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-4xl">Coming Soo00000000n okk</h1>
+  return ( 
+    <div className="min-h-screen px-24 ">
+      <MatchLive/>
+      <div className="  py-10 mt-20 ">
+        <h1 className="font-bold text-2xl text-black">
+        Leagues on Courtside 
+        </h1>
+        <div className="bg-black h-[1px] w-full my-2">
+
+        </div>
+        <div className="border bg-red-700 ">
+          <img src={img} alt="" className="h-72 w-1/3 flex justify-start items-start" />
+        </div>
+      </div>
+
     </div>
   );
 };
