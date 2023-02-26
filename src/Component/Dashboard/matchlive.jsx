657fd66931ea35c9b84e7e91e37c67d05624f16d
shadrajput <shadrajput6@gmail.com>
2023-02-26 13:00:49 +0530

    ok

--- src/Component/Dashboard/matchlive.jsx
+++ src/Component/Dashboard/matchlive.jsx
@@ -43,55 +43,55 @@ function MatchLive({ slides }) {
   };
 
   return (
-    <div className='h-full w-full '>
-      <div className='w-[100%] h-[600px] relative flex justify-start items-center overflow-hidden'>
+    <div className='h-full w-full bg-yellow-600 relative'>
+      {/* <div className='h-[261px] lg:h-[600px] relative flex justify-start items-start'> */}
         {slides.map((item, index) => {
           return (
             <div className={index === currentIndex ? 'slide active ' : 'slide'}
               key={index}>
               {index === currentIndex && (
-                <img src={item.url} alt="" className='w-full h-full bg-cover bg-center' />
+                <img src={item.url} alt="" className=' w-full  h-[250px] lg:h-full bg-cover bg-center' />
 
               )}
-              <div className='bg-gradient-to-t from-black absolute h-[750px] top-0 min-w-full   '>
-                <div className='flex flex-col justify-center items-start h-full px-32  mt-16'>
+              <div className='bg-gradient-to-t from-black absolute h-full lg:h-[750px] top-0 min-w-full   '>
+                <div className='flex flex-col justify-center items-start h-full w-full px-10 sm:px-16 lg:px-32 lg:mt-16  py-2'>
                   {
                     item.is_live == 0
                       ?
-                      <div className='bg-green-600 flex items-center pr-2 py-[3px] px-2 rounded-md text-white  '>
-                        <p className='font-semibold text-sm'>Scheduled</p>
+                      <div className='bg-green-600 flex items-center pr-2 py-[2px] lg:py-[3px] px-2 rounded-md text-white  '>
+                        <p className='font-semibold text-[10px] lg:text-sm'>Scheduled</p>
                       </div>
                       :
                       item.is_live == 1
                         ?
-                        <div className='bg-red-600 flex items-center pr-2 py-[3px] px-2 rounded-md text-white mb-2 '>
-                          <RxDotFilled className='mt-1' />
-                          <p className='font-semibold text-sm'>LIVE</p>
+                        <div className='bg-red-600 flex items-center pr-2 py-[2px] lg:py-[3px] px-2 rounded-md text-white lg:mb-2 '>
+                          <RxDotFilled className='lg:mt-1 text-sm' />
+                          <p className='font-semibold text-[10px] lg:text-sm'>LIVE</p>
                         </div>
                         :
-                        <div className='bg-black flex items-center pr-2 py-[3px] px-2 rounded-md text-white mb-2 '>
-                          <p className='font-semibold text-sm'>Finished</p>
+                        <div className='bg-black flex items-center pr-2 py-[2px] lg:py-[3px] px-2 rounded-md text-white lg:mb-2 '>
+                          <p className='font-semibold text-[10px] lg:text-sm'>Finished</p>
                         </div>
                   }
-                  <div className='flex items-start flex-col py-3'>
+                  <div className='flex items-start flex-col py-2 lg:py-3'>
                     <div className='flex items-center space-x-2'>
                       <p className='text-white text-[15px]'>{item.date} - <span>{item.day}</span> ,</p>
                       <p className='text-gray-400 text-[15px]'>{item.time}  </p>
                     </div>
-                    <p className='text-white font-bold text-lg'>{item.Tournament}</p>
+                    <p className='text-white font-bold text-sm lg:text-lg'>{item.Tournament}</p>
                   </div>
-                  <div className='flex flex-col items-center text-white gap-3 py-2'>
+                  <div className='flex flex-col items-center text-white lg:gap-3 lg:py-2'>
                     <div className="t_1  flex  justify-start w-full items-center gap-2 ">
-                      <div className="w-20 h-20 md:w-[80px] md:h-[80px]">
+                      <div className="w-8 h-8 md:w-[80px] md:h-[80px]">
                         <img src={item.Fisrt_Team_logo} className="object-contain h-full w-full rounded-full " alt="" />
                       </div>
-                      <h1 className='text-white font-extrabold text-4xl text-center uppercase'>{item.Fisrt_Team}</h1>
+                      <h1 className='text-white font-extrabold text-xs lg:text-4xl text-center uppercase'>{item.Fisrt_Team}</h1>
                     </div>
-                    <div className="t_1  flex  justify-start w-full items-center gap-2 mb-6 ">
-                      <div className="w-20 h-20 md:w-[80px] md:h-[80px]">
+                    <div className="t_1  flex  justify-start w-full items-center gap-2 ">
+                      <div className="w-8 h-8 md:w-[80px] md:h-[80px]">
                         <img src={item.Secount_Team_logo} className="object-contain h-full w-full rounded-full " alt="" />
                       </div>
-                      <h1 className='text-white font-extrabold text-4xl text-center uppercase'>{item.Secound_Team}</h1>
+                      <h1 className='text-white font-extrabold text-xs lg:text-4xl text-center uppercase'>{item.Secound_Team}</h1>
                     </div>
 
                   </div>
@@ -102,12 +102,12 @@ function MatchLive({ slides }) {
                       :
                       item.is_live == 1
                         ?
-                        <button className='bg-[#ff5000] text-white duration-300  flex gap-1 items-center  rounded-lg font-medium hover:bg-white hover:text-[#ee6730] px-20 py-[10px]'>
-                          <BsFillPlayFill className='text-2xl' />
-                          <h1 className='text-sm'>Watch</h1>
+                        <button className='bg-[#ff5000] my-2 text-white duration-300  flex gap-1 items-center  rounded-lg font-medium hover:bg-white hover:text-[#ee6730] lg:px-20 sm:px-4 sm:py-1 py-1 lg:py-[10px] px-3'>
+                          <BsFillPlayFill className='lg:text-2xl' />
+                          <h1 className='text-xs'>Watch</h1>
                         </button>
                         :
-                        <button className='bg-[#ff5000] text-white duration-300  flex gap-1 items-center  rounded-lg font-medium hover:bg-white hover:text-[#ee6730] px-20 py-[10px]'>
+                        <button className='bg-[#ff5000] text-xs my-2 text-white duration-300  flex gap-1 items-center  rounded-lg font-medium hover:bg-white hover:text-[#ee6730] lg:px-20 sm:px-4 sm:py-1 py-1 lg:py-[10px] px-3'>
                           Highlights
                         </button>
                   }
@@ -121,13 +121,13 @@ function MatchLive({ slides }) {
           slides.length > 0
             ?
             (
-              <div className='flex  absolute bottom-10 space-x-8 w-full justify-center items-center px-40 '>
+              <div className='flex  absolute bottom-3  lg:bottom-10 lg:space-x-8 space-x-7 w-full h-full items-end justify-center lg:items-center lg:px-40 '>
                 {slides.map((slide, slideIndex) => (
                   console.log(slide, "slide"),
                   <div
                     key={slideIndex}
                     onClick={() => goToSlide(slideIndex)}
-                    className={slideIndex === currentIndex ? ' text-white border-4 w-12 h-12 border-[#ee6630] rounded-full flex cursor-pointer justify-center items-center hover:text-[#ee6730] duration-300 ' : ' text-gray-500 w-12 h-12 flex border-none justify-center items-center hover:text-[#ee6730] cursor-pointer duration-300 font-bold'}
+                    className={slideIndex === currentIndex ? ' text-white lg:border-4  border-2 px-2  lg:w-12  lg:h-12 border-[#ee6630] rounded-full flex cursor-pointer justify-center items-center hover:text-[#ee6730] duration-300 ' : ' text-gray-500  lg:w-12 lg:h-12 flex border-none justify-center items-center hover:text-[#ee6730] cursor-pointer duration-300 font-bold'}
                   >
                     {slideIndex + 1}
                   </div>
@@ -139,7 +139,7 @@ function MatchLive({ slides }) {
         }
 
       </div>
-    </div>
+    // </div>
   );
 }
 
