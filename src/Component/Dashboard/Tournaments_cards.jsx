5840363257cfbfda0a52426a991dbcea29c17ae4
shadrajput <shadrajput6@gmail.com>
2023-02-28 07:07:53 +0530

    ok

--- src/Component/Dashboard/Tournaments_cards.jsx
+++ src/Component/Dashboard/Tournaments_cards.jsx
@@ -16,11 +16,11 @@ function Tournaments_cards({ tournament }) {
 
     return (
         <>
-            <div className="flex justify-center items-center bg-black hover:bg-white cursor-pointer shadow-lg xs:min-w-[120px] sm:min-w-[130px]  lg:max-w-[150] xl:w-[155px]  2xl:min-w-[158px]  h-[120px] sm:h-[120px] xl:h-[155px] 2xl:h-[150px]
+            <div className="flex justify-center items-center bg-black hover:bg-white cursor-pointer shadow-lg xs:min-w-[120px] sm:min-w-[130px]  lg:max-w-[150] xl:w-[155px]  2xl:min-w-[200px]  h-[120px] sm:h-[120px] xl:h-[155px] 2xl:h-[180px]
    group rounded-lg" onClick={handleClick}>
-                <div className='flex justify-center items-center w-full h-full '>
+                <div className='flex flex-col justify-start items-center w-full h-full '>
                     <img src={tournament.logo} alt="" className=' w-24 h-24 xl:w-32 xl:h-32 p-3' />
-
+                    <p className='text-gray-600'>NBA World cup</p>
                 </div>
             </div>
         </>
