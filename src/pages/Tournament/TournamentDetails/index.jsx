28f539e847a07539a94ac2eb078affa557abc769
SadikAli-Karadiya <karadiyasadikali@gmail.com>
2023-03-23 21:20:13 +0530

    tournament profile navbar fixed

--- src/pages/Tournament/TournamentDetails/index.jsx
+++ src/pages/Tournament/TournamentDetails/index.jsx
@@ -87,107 +87,100 @@ function TournamentDetails() {
             <h1 className="text-lg xs:text-2xl sm:text-3xl text-gray-200 font-semibold py-4">
               Champion League
             </h1>
-            {
-              tournamentdetails.status == 1
-              ?
-                <div className="w-full flex justify-center">
-                  <div className="w-40">
-                    <button
-                      onClick={handleRegisterInTournament}
-                      type="submit"
-                      className={`bg-gray-200 relative inline-flex items-center justify-center w-full px-4 py-1 xs:py-1 sm:px-4 sm:py-1.5 overflow-hidden font-medium tracking-tighter text-white rounded-lg cursor-pointer group`}
-                    >
-                      <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#ee6730] rounded-lg group-hover:w-full group-hover:h-56"></span>
-                      <span className="relative text-gray-700 text-xs xs:text-sm sm:text-base group-hover:text-white transition-all duration-500 ease-in-out">
-                        Register Your Team
-                      </span>
-                    </button>
-                  </div>
+            {tournamentdetails.status == 1 ? (
+              <div className="w-full flex justify-center">
+                <div className="w-40">
+                  <button
+                    onClick={handleRegisterInTournament}
+                    type="submit"
+                    className={`bg-gray-200 relative inline-flex items-center justify-center w-full px-4 py-1 xs:py-1 sm:px-4 sm:py-1.5 overflow-hidden font-medium tracking-tighter text-white rounded-lg cursor-pointer group`}
+                  >
+                    <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#ee6730] rounded-lg group-hover:w-full group-hover:h-56"></span>
+                    <span className="relative text-gray-700 text-xs xs:text-sm sm:text-base group-hover:text-white transition-all duration-500 ease-in-out">
+                      Register Your Team
+                    </span>
+                  </button>
                 </div>
-              :
-                null
-            }
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
       <div className="tab-container bg-[#ee6730] h-[45px] overflow-x-scroll ">
-        <div className="w-full  h-full flex justify-center items-center  px-5  space-x-5 text-white text-xs sm:text-sm lg:text-base">
-          <h1
-            className={`w-28 flex justify-center items-center  ${
-              currentTab == 0 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
-            } h-full cursor-pointer`}
-            onClick={() => setCurrentTab(0)}
-          >
-            Matches
-          </h1>
-          <h1
-            className={`w-28 flex justify-center items-center  ${
-              currentTab == 1 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
-            } h-full cursor-pointer`}
-            onClick={() => setCurrentTab(1)}
-          >
-            Teams
-          </h1>
-          <h1
-            className={`w-28 flex justify-center items-center  ${
-              currentTab == 2 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
-            } h-full cursor-pointer`}
-            onClick={() => setCurrentTab(2)}
-          >
-            Schedule
-          </h1>
-          <h1
-            className={`w-28 flex justify-center items-center  ${
-              currentTab == 3 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
-            } h-full cursor-pointer`}
-            onClick={() => setCurrentTab(3)}
-          >
-            Prize
-          </h1>
-          <h1
-            className={`w-28 flex justify-center items-center  ${
-              currentTab == 4 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
-            } h-full cursor-pointer`}
-            onClick={() => setCurrentTab(4)}
-          >
-            Sponsors
-          </h1>
-          <h1
-            className={`w-28 flex justify-center items-center  ${
-              currentTab == 5 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
-            } h-full cursor-pointer`}
-            onClick={() => setCurrentTab(5)}
-          >
-            Gallery
-          </h1>
-          <h1
-            className={`w-28 flex justify-center items-center  ${
-              currentTab == 6 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
-            } h-full cursor-pointer`}
-            onClick={() => setCurrentTab(6)}
-          >
-            About
-          </h1>
-          {
-            is_organizer
-            ?
+        <div className="w-full h-full flex items-center ">
+          <div className=" flex  items-center mx-auto h-full px-2 sm:px-3 md:px-5  lg:space-x-5 text-white text-xs sm:text-sm lg:text-base">
+            <h1
+              className={`md:w-28 sm:w-24 w-20 flex justify-center items-center  ${
+                currentTab == 0 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
+              } h-full cursor-pointer`}
+              onClick={() => setCurrentTab(0)}
+            >
+              Matches
+            </h1>
+            <h1
+              className={`md:w-28 sm:w-24 w-20 flex justify-center items-center  ${
+                currentTab == 1 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
+              } h-full cursor-pointer`}
+              onClick={() => setCurrentTab(1)}
+            >
+              Teams
+            </h1>
+            <h1
+              className={`md:w-28 sm:w-24 w-20 flex justify-center items-center  ${
+                currentTab == 2 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
+              } h-full cursor-pointer`}
+              onClick={() => setCurrentTab(2)}
+            >
+              Schedule
+            </h1>
+            <h1
+              className={`md:w-28 sm:w-24 w-20 flex justify-center items-center  ${
+                currentTab == 3 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
+              } h-full cursor-pointer`}
+              onClick={() => setCurrentTab(3)}
+            >
+              Prize
+            </h1>
+            <h1
+              className={`md:w-28 sm:w-24 w-20 flex justify-center items-center  ${
+                currentTab == 4 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
+              } h-full cursor-pointer`}
+              onClick={() => setCurrentTab(4)}
+            >
+              Sponsors
+            </h1>
+            <h1
+              className={`md:w-28 sm:w-24 w-20 flex justify-center items-center  ${
+                currentTab == 5 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
+              } h-full cursor-pointer`}
+              onClick={() => setCurrentTab(5)}
+            >
+              Gallery
+            </h1>
+            <h1
+              className={`md:w-28 sm:w-24 w-20 flex justify-center items-center  ${
+                currentTab == 6 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
+              } h-full cursor-pointer`}
+              onClick={() => setCurrentTab(6)}
+            >
+              About
+            </h1>
+            {is_organizer ? (
               <h1
-                className={`w-28 flex justify-center items-center font-semibold ${
+                className={`md:w-28 sm:w-24 w-20 flex justify-center items-center font-semibold ${
                   currentTab == 7 ? "bg-[#F5F5F7] text-[#ee6730]" : ""
                 } h-full cursor-pointer`}
                 onClick={() => setCurrentTab(7)}
               >
                 Admin*
               </h1>
-            :
-              null
-          }
+            ) : null}
+          </div>
         </div>
       </div>
       <div className="mx-auto px-8 py-10 xs:px-10 xs:py-12 sm:px-20 sm:py-12 md:px-20 md:py-16 lg:px-24 xl:px-28 2xl:px-32">
         {tabs[currentTab]}
       </div>
-
     </section>
   );
 }
