a12b0b2149491c5e593a04074df64e31a4e7c418
Deepak Prajapati <prajapatideepak073@gmail.com>
2023-03-27 21:29:48 +0530

    integrated team list :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import AppRoutes from "./routes";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-
 function App() {
   if (process.env.NODE_ENV == "development") {
     document.getElementById("root").classList.add("debug-screens");
--- src/services/team.jsx
+++ src/services/team.jsx
@@ -2,8 +2,13 @@ import { api } from "./api";
 
 export const teamApi = api.injectEndpoints({
   endpoints: (build) => ({
-   
+    getTeamList: build.query({
+      query: ({ pageNo, search }) =>
+        `team/list/${pageNo}&${
+          search == " " || search == "" ? "search" : search
+        }`,
+    }),
   }),
 });
 
-export const {} = teamApi;
+export const { useGetTeamListQuery } = teamApi;
