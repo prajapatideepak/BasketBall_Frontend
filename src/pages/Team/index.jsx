69f63058582164e78d371358f18a6630eb06acf6
Deepak Prajapati <prajapatideepak073@gmail.com>
2023-04-09 18:27:58 +0530

    created team profile page

--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -1,21 +1,25 @@
-import { lazy } from 'react'
-import { Route, Routes, Navigate } from 'react-router-dom'
-import TeamAddEdit from './TeamAddEdit'
-import TeamProfile from './TeamProfile'
-import TeamProfileDetail from './TeamProfileDetail'
-import TeamsList from './TeamsList'
+import { lazy } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import TeamAddEdit from "./TeamAddEdit";
+import TeamProfile from "./TeamProfile";
+import TeamProfileDetail from "./TeamProfileDetail";
+import TeamsList from "./TeamsList";
 
 const PrivateRoutes = () => {
   return (
     <Routes>
       <Route>
-        <Route path='/add-edit' element={<TeamAddEdit />}/>
-        <Route path='/profile' element={<TeamProfile />}/>
-        <Route path='/list' element={<TeamsList />}/>
-        <Route path='/profile-detail/:team_id' element={<TeamProfileDetail />}/>
+        <Route path="/add-edit" element={<TeamAddEdit />} />
+        <Route path="/profile" element={<TeamProfile />} />
+        <Route path="/list" element={<TeamsList />} />
+
+        <Route
+          path="/profile-detail/:team_id"
+          element={<TeamProfileDetail />}
+        />
       </Route>
     </Routes>
   );
 };
 
-export default PrivateRoutes
+export default PrivateRoutes;
