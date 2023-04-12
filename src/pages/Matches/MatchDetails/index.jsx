0ff4074b074dd4af39f6a67835eb22f429c4434a
Deepak Prajapati <prajapatideepak073@gmail.com>
2023-04-12 22:08:43 +0530

    basic repostive table done

--- src/pages/Matches/MatchDetails/index.jsx
+++ src/pages/Matches/MatchDetails/index.jsx
@@ -100,75 +100,83 @@ const MatchDetails = () => {
           <div id="scoretable" className="md:w-4/5 mx-auto text-center">
             {(data?.data?.match_data?.data?.status == 2 ||
               data?.data?.match_data?.data?.status == 3) && (
-              <div class="flex flex-col md:mt-8">
-                <div class="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative">
-                  <table class="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
+              <div className="flex flex-col md:my-8">
+                <div className="overflow-x-auto bg-white md:rounded-lg shadow overflow-y-auto relative">
+                  <table className="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
                     <thead>
-                      <tr class="text-left">
+                      <tr className="text-left">
                         <th
-                          class="py-2 px-3 sticky top-0 bg-gray-800 text-white"
-                          rowspan="2"
+                          className="py-2 px-3 sticky top-0 bg-gray-800 text-white"
+                          rowSpan="2"
                         >
                           Quarter
                         </th>
                         <th
-                          class="py-2 px-3 sticky top-0 bg-gray-800 text-white"
-                          colspan="2"
+                          className="py-2 px-3 sticky top-0 bg-gray-800 text-white"
+                          colSpan="2"
                         >
-                          <div class="flex items-center justify-center">
-                            <div class="h-4 w-4 rounded-full bg-orange-500 mr-2"></div>
+                          <div className="flex items-center justify-center">
+                            <div className="h-4 w-4 rounded-full bg-orange-500 mr-2"></div>
                             {data?.data?.match_data?.data?.team_1.team_name}
                           </div>
                         </th>
                         <th
-                          class="py-2 px-3 sticky top-0 bg-gray-800 text-white"
-                          colspan="2"
+                          className="py-2 px-3 sticky top-0 bg-gray-800 text-white"
+                          colSpan="2"
                         >
-                          <div class="flex items-center justify-center">
-                            <div class="h-4 w-4 rounded-full bg-blue-500 mr-2"></div>
+                          <div className="flex items-center justify-center">
+                            <div className="h-4 w-4 rounded-full bg-blue-500 mr-2"></div>
                             {data?.data?.match_data?.data?.team_2.team_name}
                           </div>
                         </th>
                         <th
-                          class="py-2 px-3 sticky top-0 bg-gray-800 text-white"
-                          rowspan="2"
+                          className="py-2 px-3 sticky top-0 bg-gray-800 text-white"
+                          rowSpan="2"
                         >
                           Winner
                         </th>
                       </tr>
                       <tr>
-                        <th class="bg-gray-700 text-white px-3 py-2">Fouls</th>
-                        <th class="bg-gray-700 text-white px-3 py-2">Points</th>
-                        <th class="bg-gray-700 text-white px-3 py-2">Fouls</th>
-                        <th class="bg-gray-700 text-white px-3 py-2">Points</th>
+                        <th className="bg-gray-700 text-white px-3 py-2 font-medium tracking-wide">
+                          Fouls
+                        </th>
+                        <th className="bg-gray-700 text-white px-3 py-2 font-medium tracking-wide">
+                          Points
+                        </th>
+                        <th className="bg-gray-700 text-white px-3 py-2 font-medium tracking-wide">
+                          Fouls
+                        </th>
+                        <th className="bg-gray-700 text-white px-3 py-2 font-medium tracking-wide">
+                          Points
+                        </th>
                       </tr>
                     </thead>
                     <tbody>
                       {data?.data?.match_data?.all_quarters?.map((q) => {
                         return (
                           <tr key={q?.id}>
-                            <td class="border-t px-3 py-2">
+                            <td className="border-t px-3 py-2">
                               {q?.quarter_number}
                             </td>
-                            <td class="border-t px-3 py-2">
+                            <td className="border-t px-3 py-2 sm:text-sm lg:text-base">
                               {q?.team_1_fouls}
                             </td>
-                            <td class="border-t px-3 py-2">
+                            <td className="border-t px-3 py-2 sm:text-sm lg:text-base">
                               {q?.team_1_points}
                             </td>
-                            <td class="border-t px-3 py-2">
+                            <td className="border-t px-3 py-2 sm:text-sm lg:text-base">
                               {q?.team_2_fouls}
                             </td>
-                            <td class="border-t px-3 py-2">
+                            <td className="border-t px-3 py-2 sm:text-sm lg:text-base">
                               {q?.team_2_points}
                             </td>
-                            <td class="border-t px-3 py-2">
+                            <td className="border-t px-3 py-2 sm:text-sm lg:text-base">
                               {q?.status !== 0 ? (
                                 "--"
                               ) : (
-                                <div class="flex items-center justify-center">
+                                <div className="flex items-center  justify-center">
                                   <div
-                                    class={`h-4 w-4 rounded-full bg-${
+                                    className={`h-4 w-4 rounded-full bg-${
                                       q?.won_by_team_id ==
                                       data?.data?.match_data?.data?.team_1.id
                                         ? "orange"
@@ -178,9 +186,9 @@ const MatchDetails = () => {
                                   {q?.won_by_team_id ==
                                   data?.data?.match_data?.data?.team_1.id
                                     ? data?.data?.match_data?.data?.team_1
-                                        ?.team_name
+                                        .team_name
                                     : data?.data?.match_data?.data?.team_2
-                                        ?.team_name}
+                                        .team_name}
                                 </div>
                               )}
                             </td>
